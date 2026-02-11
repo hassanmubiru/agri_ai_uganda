eb10831f9c180c80216bec9ed683f1dd8ca5b5cb
hassanmubiru <hassanmubiru7@gmail.com>
2026-02-11 05:46:36 +0300

    Changes to be committed: 	modified:   android/app/src/main/AndroidManifest.xml 	new file:   assets/models/labels.txt 	new file:   assets/models/model.tflite 	modified:   ios/Runner/Info.plist 	modified:   lib/main.dart 	new file:   lib/screens/home_screen.dart 	new file:   lib/utils/constants.dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryGreen = Color(0xFF4CAF50);
+  static const Color darkGreen = Color(0xFF2E7D32);
+  static const Color earthBrown = Color(0xFF8D6E63);
+  static const Color darkBrown = Color(0xFF5D4037);
+  static const Color accentYellow = Color(0xFFFFC107);
+  static const Color backgroundLight = Color(0xFFF1F8E9);
+  static const Color backgroundDark = Color(0xFF1B5E20);
+}
+
+class AppStrings {
+  static const String appName = 'AgriAI Uganda';
+  static const String home = 'Home';
+  static const String detect = 'Detect';
+  static const String water = 'Water';
+  static const String market = 'Market';
+  static const String history = 'History';
+}
