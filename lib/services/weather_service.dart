74f5d436447264bf227eec76478fe9b33aaafe9b
hassanmubiru <hassanmubiru7@gmail.com>
2026-02-11 05:55:40 +0300

    Changes to be committed: 	modified:   lib/services/weather_service.dart 	modified:   test/widget_test.dart

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:geolocator/geolocator.dart';
-import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:http/http.dart' as http;
+// import 'package:shared_preferences/shared_preferences.dart';
 
 class WeatherService {
   // Placeholder API Key - In production, use env variables
