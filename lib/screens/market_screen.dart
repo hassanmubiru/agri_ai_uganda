c6ef56b77900ce4e3d83c5e706e36ad59b8c4bf6
hassanmubiru <hassanmubiru7@gmail.com>
2026-02-12 05:21:34 +0300

    Changes to be committed: 	new file:   android/build/reports/problems/problems-report.html 	modified:   lib/screens/market_screen.dart 	modified:   pubspec.yaml

--- lib/screens/market_screen.dart
+++ lib/screens/market_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:community_charts_flutter/community_charts_flutter.dart' as charts;
 import 'package:agri_ai_uganda/services/market_service.dart';
 import 'package:agri_ai_uganda/models/market_price.dart';
 import 'package:agri_ai_uganda/utils/constants.dart';
